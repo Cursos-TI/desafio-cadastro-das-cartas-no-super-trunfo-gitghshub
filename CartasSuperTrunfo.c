1fe0d642ab77c1f7840fd1efa6d99bff82c197e3
gitghshub <gustavohenriquecred@gmail.com>
2025-08-04 19:16:21 +0000

    novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -71,6 +71,8 @@ int main() {
     printf("Área: %.2f km²\n", area2);
     printf("PIB: R$ %.2f\n", pib2);
     printf("Pontos Turísticos: %d\n", pontosTuristicos2);
+    printf("novo commit\n"),
+    
     
     return 0;
 }
