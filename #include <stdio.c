48a259f7fc997cfd8b8e7ae463105efea5ab3189
gitghshub <gustavohenriquecred@gmail.com>
2025-10-22 00:50:20 +0000

    criei o printf

---
+++ #include <stdio.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main () {
+
+    int i = 1;
+
+    printf("***Torre move-se***\n");
+    do { 
+       printf("Para frente!\n"); // torre move 5 vezes para frente.
+     i++; // evita o loop infinito.
+   } while (i <= 5);
+
+   // reiniciando
+   int j = 1;
+
+   printf("***Rainha move-se*** \n");
+    while (j <= 8) {
+    printf("Para esquerda!\n"); // rainha move 8 casas para esquerda.
+    j++; 
+}
+
+    printf("***Bispo move-se***\n");
+    for (i = 1; i <= 5; i++) // Bispo move-se 5 casas para diagonal direita.
+    {   
+    printf("%d - Diagonal-Direita\n", i);
+    }
+
+return 0;
+}
